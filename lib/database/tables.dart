b89caa3f5f17a7c90e26793a23ad9022bce411db
Cauã Cavalcante Gonçalves <gonssalvescaua@gmail.com>
2025-08-07 15:51:16 -0300

    Update tables.dart

--- lib/database/tables.dart
+++ lib/database/tables.dart
@@ -1,11 +1,8 @@
 import 'package:drift/drift.dart';
 
-// Nome da classe é no plural (Games)
 class Games extends Table {
-  // id será a chave primária, auto-incrementada
   IntColumn get id => integer().autoIncrement()();
   
-  // Colunas para guardar as informações do jogo
   TextColumn get name => text()();
   TextColumn get coverUrl => text()(); // O mais importante para resolver seu bug!
   TextColumn get platform => text()();
